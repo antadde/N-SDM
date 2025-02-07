9ab86a43c6c5ad1e146b44d5a35d418b98a79aa6
N-SDM <adde.antoine@gmail.com>
2025-02-07 15:49:04 +0100

    Update nsdm.map.R

--- scripts/nsdm-project/functions_nsdm2/R/nsdm.map.R
+++ scripts/nsdm-project/functions_nsdm2/R/nsdm.map.R
@@ -1,47 +1,72 @@
 #' nsdm.map
 #'
-#' Fill empty template rasters with predicted values
+#' Fill an empty raster template with predicted values.
 #'
-#' @param template An empty raster layer to fill with predicted values
-#' @param nona_ix Numeric indices of cells that should be filled with predicted values
-#' @param pred List containing vectors of fitted values (one list by modelling algorithm)
-#' @param species_name A character string indicating the name of the taxon for which models are fitted
-#' @param model_name A character vector indicating the name of the modelling algorithm(s) for which predicted values are provided
-#' @param level A character vector indicating the name of the level considered (e.g., glo or loc)
-#' @param nesting_name A character vector indicating the name of the nesting strategy for which predicted values are provided
-#' @param scenar_name A character vector indicating the name of the scenario for which predicted values are provided
-#' @param period_name A character vector indicating the name of the period for which predicted values are provided
+#' @param template An empty raster layer to fill with predicted values.
+#' @param nona_ix Numeric indices of cells that should be filled with predicted values.
+#' @param pred A list where each element corresponds to a modeling algorithm and contains vectors of fitted values.
+#' @param species_name A character string indicating the name of the taxon for which models are fitted.
+#' @param model_name A character string indicating the name of the modeling algorithm used.
+#' @param level A character string indicating the spatial level considered (e.g., "glo" for global, "loc" for local).
+#' @param nesting_name A character string indicating the nesting strategy for which predicted values are provided.
+#' @param scenar_name A character string indicating the scenario for which predicted values are provided.
+#' @param period_name A character string indicating the time period for which predicted values are provided.
 #'
-#' @return A list of rasters filled with predicted values
-#' @author Antoine Adde (aadde@unil.ch)
+#' @return A list of rasters filled with predicted values (for ESM models) or a single raster (for other models).
+#' @author Antoine Adde (antoine.adde@eawag.ch)
 #' @export
 
-nsdm.map<-function(template, nona_ix, pred, species_name, model_name, level=NA, nesting_name=NA, scenar_name=NA, period_name=NA){
+nsdm.map <- function(template, nona_ix, pred, species_name, model_name, level = NA, nesting_name = NA, scenar_name = NA, period_name = NA) {
 
-pred_fit_f<-list() # list where results will be stored
-  
-for(m in 1:length(pred)){ # Loop over models (1 in regular cases but much more in esm settings)
-    
-# Retrieve model fit
-if(model_name=="esm")fit<-pred[[m]]$fit
-if(model_name!="esm")fit<-pred$fit
+  pred_fit_f <- list()  # List to store results
 
-# In case of esm model uses its name its index for naming
-if(model_name=="esm"){
-model_nameu<-names(pred)[m]
-}else{
-model_nameu<-model_name}    
+  # Ensure pred is not empty before looping
+  if (length(pred) == 0) {
+    warning("Input 'pred' is empty. Returning NULL.")
+    return(NULL)
+  }
 
-# Fill template rasters
-pred_fit<-template
-pred_fit[]<-NA
-pred_fit[nona_ix]<-round(fit*100)
-names(pred_fit)<-gsub("_NA", "", paste(species_name, model_nameu, level, nesting_name, scenar_name, period_name, sep="_"))
-storage.mode(pred_fit[]) = "integer"
+  for (m in seq_along(pred)) {  # Safe loop indexing
 
-pred_fit_f[[m]]<-pred_fit
-names(pred_fit_f)[m]<-model_nameu
+    # Retrieve model fit
+    if (model_name == "esm") {
+      if (!is.null(pred[[m]]$fit)) {
+        fit <- pred[[m]]$fit
+      } else {
+        warning(paste("Missing 'fit' in ESM model", m))
+        next  # Skip this iteration if 'fit' is missing
+      }
+    } else {
+      if (!is.null(pred$fit)) {
+        fit <- pred$fit
+      } else {
+        stop("Error: 'fit' not found in 'pred' for non-ESM model.")
+      }
+    }
 
+    # Determine model-specific naming
+    model_nameu <- if (model_name == "esm" && !is.null(names(pred)[m])) names(pred)[m] else model_name
+
+    # Fill template raster
+    pred_fit <- template
+    pred_fit[] <- NA
+    pred_fit[nona_ix] <- round(fit * 100)
+
+    # Construct raster name safely
+    pred_fit_name <- gsub("_NA", "", paste(species_name, model_nameu, level, nesting_name, scenar_name, period_name, sep = "_"))
+    names(pred_fit) <- pred_fit_name
+
+    storage.mode(pred_fit[]) <- "integer"  # Ensure integer format
+
+    # Store result
+    pred_fit_f[[m]] <- pred_fit
+    names(pred_fit_f)[m] <- model_nameu
+  }
+
+  # Return full list for ESM models, single output for others
+  if (model_name == "esm") {
+    return(pred_fit_f)
+  } else {
+    return(pred_fit_f[[1]])
+  }
 }
-if(model_name=="esm")return(pred_fit_f)
-if(model_name!="esm")return(pred_fit_f[[1]])}
