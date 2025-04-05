9635a628ab1dc83e92795e6e1178d3c742b026b5
Adde <109168567+antadde@users.noreply.github.com>
2025-04-05 09:58:25 +0200

    Update nsdm.pseudoabsences_class.R

--- scripts/nsdm-project/functions_nsdm2/R/nsdm.pseudoabsences_class.R
+++ scripts/nsdm-project/functions_nsdm2/R/nsdm.pseudoabsences_class.R
@@ -2,9 +2,10 @@
 #'
 #' Not to be called directly by the user
 #'
-#' @author Philipp Brun (philipp.brun@wsl.ch)
+#' @author Philipp Brun (philipp.brun@wsl.ch) and Antoine Adde (antoine.adde@eawag.ch)
 #' @export
 nsdm.pseudoabsences<-setClass("nsdm.pseudoabsences",slots=c(meta="list", # Meta information
+                                                            level="character", # Global or Regional
                                                             pa="numeric", # store presence/pseudoabsence information
 															years="numeric", # store year information
                                                             env_vars="data.frame", # store extracted env variables
