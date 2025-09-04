8a98c7fd1a6b2fc64692353cb0152bb12df22ac5
Adde <109168567+antadde@users.noreply.github.com>
2025-09-04 23:54:13 +0200

    add nsdm.fitting

--- scripts/nsdm-project/functions_nsdm2/R/nsdm.fit.R
+++ scripts/nsdm-project/functions_nsdm2/R/nsdm.fit.R
@@ -1,128 +1,147 @@
-#' nsdm.fit
+#' nsdm.fitting
 #'
 #' Model fitting
 #'
-#' @param x A nsdm.pseudoabsences object
-#' @param sets
-#' @param mod_args List of class 'multi.input' containing information on models to be fitted
-#' @param level A charachter string indicating the level evaluated (e.g. glo or reg)
-#' @param ncores Number of cores to be used during parallel operations
-#' @param tmp_path Character indicating the path where to store temporary outputs
+#' @md
+#' @param x An `nsdm.pseudoabsences` object used as the master table for feature values and SIDs
+#' @param sets A named list of replicates produced upstream, each replicate contains `reg_train`, `reg_test`, `glo_train`, `glo_test`
+#' @param mod_args A list of class `multi.input`, one element per model family, each with slots `@mod` a function name, `@args` a list of arguments, `@weight` logical, `@tag` a short label
+#' @param level Character string, `"glo"` or `"reg"`, selects which subset of `sets` to use for SIDs
+#' @param ncores Integer, number of cores for `mclapply`
+#' @param tmp_path Character, directory for temporary files such as LightGBM saves
 #'
-#' @return A nsdm.fit object including slots for meta info, testing data for evaluation, and model objects
+#' @return An `nsdm.fit` object with fitted models stored in `@fits`, grouped by `@tag`, each tag holds a list of per replicate fits
 #' @author Antoine Adde (antoine.adde@eawag.ch)
 #' @export
+nsdm.fitting <- function(x,
+                         sets,
+                         mod_args,
+                         level,
+                         ncores = 1L,
+                         tmp_path) {
 
-nsdm.fit<-function(x,
-                   sets,
-                   mod_args,
-				   level,
-				   ncores,
-				   tmp_path){
+  reps  <- length(sets)
 
+  # container for all model families across replicates
+  modis <- list()
 
-  # Check sets
-  reps = length(sets)
-  
-  # loop over model types
-  for(j in 1:length(mod_args)){
-   
-  mod_args_j=mod_args[[j]]
-  
-  # parallelize over replicates
- modis=list()
- modit <-mclapply(1:reps, function(rep_id){
-  
-  # Retrieve replicate values
-sub_sets  <- sets[[rep_id]][grep(level, names(sets[[rep_id]]))]
-train_sid <- sub_sets[[paste0(level, "_train")]]@sid
-test_sid  <- sub_sets[[paste0(level,  "_test")]]@sid
+  # loop over model families
+  for (j in seq_along(mod_args)) {
 
-## build train and test rows from x
-i_tr <- match(train_sid, x@sid); i_tr <- i_tr[!is.na(i_tr)]
-i_te <- match(test_sid,  x@sid); i_te <- i_te[!is.na(i_te)]
+    mod_args_j <- mod_args[[j]]
 
-train <- cbind(
-  data.frame(sid = x@sid[i_tr], split = "train", Presence = x@pa[i_tr]),
-  x@env_vars[i_tr, , drop = FALSE]
-)
-test <- cbind(
-  data.frame(sid = x@sid[i_te], split = "test", Presence = x@pa[i_te]),
-  x@env_vars[i_te, , drop = FALSE]
-)
+    # fit in parallel across replicates
+    modit <- parallel::mclapply(seq_len(reps), function(rep_id) {
 
-## only presence and env vars
-df_train <- train[, c("Presence", colnames(x@env_vars)), drop = FALSE]
-df_test <- test[, c("Presence", colnames(x@env_vars)), drop = FALSE]
+      # fetch train and test SIDs for this level and replicate
+      sub_sets  <- sets[[rep_id]][grep(level, names(sets[[rep_id]]))]
+      train_sid <- sub_sets[[paste0(level, "_train")]]@sid
+      test_sid  <- sub_sets[[paste0(level,  "_test")]]@sid
 
+      # index rows in x
+      i_tr <- match(train_sid, x@sid); i_tr <- i_tr[!is.na(i_tr)]
+      i_te <- match(test_sid,  x@sid); i_te <- i_te[!is.na(i_te)]
 
-  ## Weights
-  if(mod_args_j@weight){
-  wi=which(df_train$Presence==1)
-  wt=rep(1, nrow(df_train))
-  wt[wi]<-round((length(wt)-length(wi))/length(wi))
+      # build train and test frames
+      train <- cbind(
+        data.frame(sid = x@sid[i_tr], split = "train", Presence = x@pa[i_tr]),
+        x@env_vars[i_tr, , drop = FALSE]
+      )
+      test <- cbind(
+        data.frame(sid = x@sid[i_te], split = "test", Presence = x@pa[i_te]),
+        x@env_vars[i_te, , drop = FALSE]
+      )
+
+      df_train <- train[, c("Presence", colnames(x@env_vars)), drop = FALSE]
+      df_test  <- test[,  c("Presence", colnames(x@env_vars)), drop = FALSE]  # reserved if you later add evaluation
+
+      # weights for presence upweighting if requested
+      wt <- NULL
+      if (isTRUE(mod_args_j@weight)) {
+        wi <- which(df_train$Presence == 1)
+        wt <- rep(1, nrow(df_train))
+        if (length(wi) > 0 && length(wi) < length(wt)) {
+          wt[wi] <- max(1L, round((length(wt) - length(wi)) / length(wi)))
+        }
+      }
+
+      # choose model engine
+      if (identical(mod_args_j@mod, "maxnet")) {
+        # Maxent like with maxnet
+        mod_args_j@args$data <- train[, colnames(x@env_vars), drop = FALSE]
+        mod_args_j@args$p    <- df_train$Presence
+        fit <- try(do.call(mod_args_j@mod, mod_args_j@args), silent = TRUE)
+        if (inherits(fit, "try-error")) {
+          mod_args_bis <- mod_args_j@args
+          mod_args_bis$addsamplestobackground <- TRUE
+          fit <- do.call(mod_args_j@mod, mod_args_bis)
+        }
+
+      } else if (identical(mod_args_j@mod, "lgb.train")) {
+        # LightGBM
+        taxon <- tryCatch(x@meta$taxon, error = function(e) "unknown_taxon")
+        tmp_path_gbm <- file.path(tmp_path, "gbm")
+        if (!dir.exists(tmp_path_gbm)) dir.create(tmp_path_gbm, recursive = TRUE)
+
+        if (!is.null(wt)) {
+          mod_args_j@args$data <- lightgbm::lgb.Dataset(
+            data  = as.matrix(df_train[, colnames(x@env_vars), drop = FALSE]),
+            label = df_train$Presence,
+            weight = wt
+          )
+        } else {
+          mod_args_j@args$data <- lightgbm::lgb.Dataset(
+            data  = as.matrix(df_train[, colnames(x@env_vars), drop = FALSE]),
+            label = df_train$Presence
+          )
+        }
+
+        fit <- do.call(mod_args_j@mod, mod_args_j@args)
+
+        # save LightGBM model to disk
+        safe_taxon <- gsub("[^A-Za-z0-9_]+", "_", taxon)
+        out_file <- file.path(
+          tmp_path_gbm,
+          sprintf("%s_rep%02d_%s_%s.txt", safe_taxon, rep_id, level, mod_args_j@tag)
+        )
+        lightgbm::lgb.save(fit, out_file)
+
+      } else if (mod_args_j@mod %in% c("glm", "mgcv_gam", "mgcv_fx", "esm")) {
+        # GLM or GAM family
+        if (!is.null(wt)) mod_args_j@args$weights <- wt
+        mod_args_j@args$data <- df_train
+        fit <- do.call(mod_args_j@mod, mod_args_j@args)
+
+      } else if (identical(mod_args_j@mod, "randomForest")) {
+        # randomForest
+        if (!is.null(wt)) mod_args_j@args$weights <- wt
+        mod_args_j@args$data <- df_train[, colnames(x@env_vars), drop = FALSE]
+        mod_args_j@args$data$Presence <- as.factor(df_train$Presence)
+        fit <- do.call(mod_args_j@mod, mod_args_j@args)
+
+      } else if (identical(mod_args_j@mod, "ranger")) {
+        # ranger
+        if (!is.null(wt)) mod_args_j@args$case.weights <- wt
+        mod_args_j@args$data <- df_train[, colnames(x@env_vars), drop = FALSE]
+        mod_args_j@args$data$Presence <- as.factor(df_train$Presence)
+        fit <- do.call(mod_args_j@mod, mod_args_j@args)
+
+      } else {
+        stop(sprintf("Unknown model engine: %s", mod_args_j@mod))
+      }
+
+      return(fit)
+    }, mc.cores = ncores)
+
+    # name replicates
+    names(modit) <- sprintf("replicate_%02d", seq_len(reps))
+
+    # store under model tag
+    modis[[mod_args_j@tag]] <- modit
   }
-  
-  ### MAX
-  if(mod_args_j@mod=="maxnet"){
-  mod_args_j@args$data<-train[, c(colnames(x@env_vars)), drop = FALSE]
-  mod_args_j@args$p<-df_train$Presence
-  modi<-try(do.call(mod_args_j@mod, mod_args_j@args), TRUE)
-  if("try-error" %in% class(modi)){
-  mod_args_bis<-mod_args_j@args
-  mod_args_bis$addsamplestobackground<-TRUE
-  modi<-do.call(mod_args_j@mod, mod_args_bis)}}
 
-  ### GBM
- if(mod_args_j@mod=="lgb.train"){
-  tmp_path_gbm<-paste0(tmp_path, "/gbm")
-  dir.create(tmp_path_gbm, recursive=TRUE)
-  if(mod_args_j@weight){
-  mod_args_j@args$data<-lgb.Dataset(as.matrix(df_train[, c(colnames(x@env_vars)), drop = FALSE]), 
-                                    label = df_train$Presence, weight=wt)
-  } else {
-  mod_args_j@args$data<-lgb.Dataset(as.matrix(df_train[, c(colnames(x@env_vars)), drop = FALSE]), 
-                                    label = df_train$Presence)}  
-									
-  modi<-do.call(mod_args_j@mod,mod_args_j@args)
-  lgb.save(modi, paste0(tmp_path_gbm,"/",taxon,"_rep",x,"_mod",j,"_",level,".rds"))}
-  
-  ### GLM or GAM
-  if(mod_args_j@mod %in% c("glm", "mgcv_gam", "mgcv_fx", "esm")){
-  if(mod_args_j@weight) mod_args_j@args$weights=wt
-  mod_args_j@args$data=df_train
-  modi=do.call(mod_args_j@mod, mod_args_j@args)
-  }
-  
-  ### RF
-  if(mod_args_j@mod=="randomForest"){
-  if(mod_args_j@weight) mod_args_j@args$weights=wt
-   mod_args_j@args$data=df_train[, c(colnames(x@env_vars)), drop = FALSE]
-   mod_args_j@args$data$Presence=as.factor(df_train$Presence)
-   modi=do.call(mod_args_j@mod, mod_args_j@args)}
-   
-   ### RF with Ranger
-  if(mod_args_j@mod=="ranger"){
-  if(mod_args_j@weight) mod_args_j@args$case.weights=wt
-   mod_args_j@args$data=df_train[, c(colnames(x@env_vars)), drop = FALSE]
-   mod_args_j@args$data$Presence=as.factor(df_train$Presence)
-   modi=do.call(mod_args_j@mod, mod_args_j@args)}
-   
- 
-  return(modi)}, mc.cores=ncores)
-  
-# Rename replicates
-names(modit)=paste0("replicate_",sprintf("%02d",1:reps))
-
-# Supply fitted replicates
-modis[[mod_args_j@tag]]<-modit
-}
-
-# supply fitted objects
-# Initiate out object
+  # wrap into nsdm.fit object
   out <- nsdm.fit()
-  lis <- list(nsdm.i = out)
-  lis$nsdm.i@fits=modis
-
-return(lis$nsdm.i)
+  out@fits <- modis
+  return(out)
 }
