4713dcad10787ed31fdf134cb4e350fe9a36854b
Adde <109168567+antadde@users.noreply.github.com>
2025-10-08 09:18:33 +0200

    Save table outputs as .psv instead of .rds

--- Rpkg/R/nsdm.loadthis.R
+++ Rpkg/R/nsdm.loadthis.R
@@ -1,26 +1,51 @@
 #' nsdm.loadthis
 #'
-#' Easy loading of nsdm.ine objects
+#' Easy loading of nsdm objects (.rds or .psv)
 #'
-#' @param model_name A character string indicating the name of the modelling alogirthm related to the target object to be loaded
-#' @param species_name  A character string  indicating the name of the species related to the target object to be loaded
-#' @param tag  A character string indicating the tag for the hyperparameter X modelling algorithm combination of the target object to be loaded
+#' @param model_name A character string indicating the name of the modelling algorithm related to the target object to be loaded
+#' @param species_name A character string indicating the name of the species related to the target object to be loaded
+#' @param tag A character string indicating the tag for the hyperparameter X modelling algorithm combination of the target object to be loaded
 #' @param read_path A character string indicating the upper path where the target object is stored (above model and species)
+#' @param format A character string, either "rds" (default) or "psv", specifying the file format to load
 #'
-#' @return The desired nsdm.ine object
-#' @author Antoine Adde (aadde@unil.ch)
+#' @return The desired nsdm object
+#' @author Antoine Adde (antoine.adde@eawag.ch)
 #' @export
-nsdm.loadthis<-function(model_name=NULL, species_name=NULL, tag=NULL, read_path){
+nsdm.loadthis <- function(model_name = NULL,
+                          species_name = NULL,
+                          tag = NULL,
+                          read_path,
+                          format = "rds") {
 
-read_this_path<-paste(read_path, species_name, model_name, sep="/")
+  # Construct path to object
+  read_this_path <- paste(read_path, species_name, model_name, sep = "/")
 
-if(is.null(tag)){f<-gsub("_\\.",".",paste0(read_this_path,"/", paste(species_name,model_name,sep="_"),".rds"))
-}else{f<-gsub("_\\.",".",paste0(read_this_path,"/", paste(species_name,tag,sep="_"),".rds"))}
+  # Build base filename
+  if (is.null(tag)) {
+    base_name <- paste(species_name, model_name, sep = "_")
+  } else {
+    base_name <- paste(species_name, tag, sep = "_")
+  }
 
-#if(model_name=="gbm"){
-#readRDS.lgb.Booster(f)
-#}else{
-readRDS(f)
-#}
+  # Normalize potential underscores before dot
+  file_rds <- gsub("_\\.", ".", paste0(read_this_path, "/", base_name, ".rds"))
+  file_psv <- gsub("_\\.", ".", paste0(read_this_path, "/", base_name, ".psv"))
 
+  # Determine which file to load
+  if (tolower(format) == "psv" && file.exists(file_psv)) {
+    object <- read.table(file_psv, sep = "|", header = TRUE, stringsAsFactors = FALSE)
+
+  } else if (file.exists(file_rds)) {
+    object <- readRDS(file_rds)
+
+  } else if (file.exists(file_psv)) {
+    # Fallback if .rds not found but .psv exists
+    object <- read.table(file_psv, sep = "|", header = TRUE, stringsAsFactors = FALSE)
+    warning("RDS file not found; loaded PSV file instead.")
+
+  } else {
+    stop("No valid file found for: ", base_name)
+  }
+
+  return(object)
 }
--- Rpkg/R/nsdm.savethis.R
+++ Rpkg/R/nsdm.savethis.R
@@ -1,24 +1,59 @@
 #' nsdm.savethis
 #'
-#' Easy saving of .rds nsdm.ine objects
+#' Easy saving of .rds or .psv objects
 #'
 #' @param object Object to be saved
 #' @param model_name A character string indicating the name of the target modelling algorithm
-#' @param species_name A character string  indicating the name of the target taxon
-#' @param tag A character string  indicating the tag for the target hyperparameter X modelling algorithm combination
-#' @param compression Logical (TRUE FALSE) for saveRDS "compress" argument
+#' @param species_name A character string indicating the name of the target taxon
+#' @param tag A character string indicating the tag for the target hyperparameter X modelling algorithm combination
+#' @param compression Logical (TRUE/FALSE) for saveRDS "compress" argument
 #' @param save_path A character string indicating the upper path where to save the object
+#' @param format A character string, either "rds" (default) or "psv", specifying the output format
 #'
-#' @return Saved object in .rds format
-#' @author Antoine Adde (aadde@unil.ch)
+#' @return Saved object in .rds or .psv format
+#' @author Antoine Adde (antoine.adde@eawag.ch)
 #' @export
+nsdm.savethis <- function(object,
+                          model_name = NULL,
+                          species_name = NULL,
+                          tag = NULL,
+                          compression = FALSE,
+                          save_path,
+                          format = "rds") {
 
-nsdm.savethis<-function(object, model_name=NULL, species_name=NULL, tag=NULL, compression=FALSE, save_path){
-save_this_path<-paste(save_path, species_name, model_name, sep="/")
-suppressWarnings(dir.create(save_this_path,  recursive = TRUE))
-if(is.null(tag)){f<-gsub("_\\.",".",paste0(save_this_path,"/", paste(species_name,model_name,sep="_"),".rds"))
-}else{f<-gsub("_\\.",".",paste0(save_this_path,"/", paste(species_name,tag,sep="_"),".rds"))}
-if("lgb.Booster" %in% class(object)){
-lgb.save(object, file=f)
-}else{saveRDS(object, file=f, compress=compression)}
+  # Create save directory
+  save_this_path <- paste(save_path, species_name, model_name, sep = "/")
+  suppressWarnings(dir.create(save_this_path, recursive = TRUE))
+
+  # Define output filename (without extension)
+  if (is.null(tag)) {
+    base_name <- paste(species_name, model_name, sep = "_")
+  } else {
+    base_name <- paste(species_name, tag, sep = "_")
+  }
+
+  # Handle GBM separately
+  if ("lgb.Booster" %in% class(object)) {
+    f <- paste0(save_this_path, "/", base_name, ".rds")
+    lgb.save(object, file = f)
+
+  } else if (tolower(format) == "psv") {
+    # Save as PSV (pipe-separated values)
+    f <- paste0(save_this_path, "/", base_name, ".psv")
+
+    if (is.data.frame(object)) {
+      write.table(object, file = f, sep = "|", quote = FALSE, row.names = FALSE)
+    } else {
+      warning("PSV format is intended for data frames; object saved as RDS instead.")
+      f <- paste0(save_this_path, "/", base_name, ".rds")
+      saveRDS(object, file = f, compress = compression)
+    }
+
+  } else {
+    # Default RDS saving
+    f <- paste0(save_this_path, "/", base_name, ".rds")
+    saveRDS(object, file = f, compress = compression)
+  }
+
+  return(invisible(f))
 }
